ae7c31b31900029e611045f5edad122a089f189f
Saurabh Sagar <ssnl1216sagarsaurabh@gmail.com>
2025-07-31 20:29:40 +0000

    i think you should increase the welcome box a little more -the image is

--- src/components/home/welcome-banner.tsx
+++ src/components/home/welcome-banner.tsx
@@ -10,7 +10,7 @@ export default function WelcomeBanner() {
             Your journey to IIT Delhi starts now. Seize the day!
           </p>
         </div>
-        <div className="relative h-48 md:h-full">
+        <div className="relative h-60 md:h-full">
            <Image
               src="https://raw.githubusercontent.com/sagar-0016/Pranjal-Study-Buddy/refs/heads/main/IIT%20Delhi%20Emblem%20in%20Serene%20Landscape.png"
               alt="IIT Delhi Emblem in a serene landscape"
