4558d3153802009cb9549f47c934f44bd8f11c80
Saurabh Sagar <ssnl1216sagarsaurabh@gmail.com>
2025-08-18 09:50:11 +0000

    local one is again gone

--- next.config.ts
+++ next.config.ts
@@ -34,7 +34,13 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'study-buddy-two-phi.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
