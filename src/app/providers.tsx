9a16ddaeef7f61cdb5c920764e83c031704e92db
Saurabh Sagar <ssnl1216sagarsaurabh@gmail.com>
2025-09-09 17:26:01 +0000

    the background still seems a little greenish, can you change it to bluis

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -37,7 +37,7 @@ function AppBackground() {
 
   // Default grid background
   return (
-    <div className="fixed inset-0 -z-10 h-full w-full bg-green-50 dark:bg-black">
+    <div className="fixed inset-0 -z-10 h-full w-full bg-blue-50 dark:bg-black">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,rgba(255,255,255,0.12)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.12)_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
     </div>
   );
